aa59d46fc94532da122b30bf9b621bcf39969b2b
sai susheel gupta s <54953337+saisusheelguptas27072002@users.noreply.github.com>
2021-06-06 21:35:22 +0530

    fullstackopen-Exercise 2.1-2.20

---
+++ P-2.16/src/components/Persons.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+
+const Persons = ({persons, searchName, deletePerson}) => {
+    return (
+        persons.length ?
+        <ul style={{listStyle: 'none', padding: 0}}>
+            {
+                persons.filter(person => person.name.toLowerCase().includes(searchName.toLowerCase()))
+                .map(person => (
+                    <li key={person.name}>
+                        {person.name} {person.number} &nbsp;
+                        <button onClick={() => deletePerson(person.id, person.name)}>Delete</button>
+                    </li>
+                ))
+            }
+        </ul>
+        :
+        <p>Loading contacts...</p>
+    );
+};
+
+export default Persons;
